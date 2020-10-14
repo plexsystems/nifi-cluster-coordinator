cc543fb72e8c7a61e9648d505051e33b232cc61f
PLEXEMP\gsingh <gsingh@plex.com>
2020-10-14 12:00:01 -0400

    * Created parameter context service to synchronize parameter contexts
    * Updated enviroment service to use defined parameter context for underlying process group
    
    * Updated log comments to be more specific

--- nifi_cluster_coordinator/configuration/config_loader.py
+++ nifi_cluster_coordinator/configuration/config_loader.py
@@ -3,14 +3,32 @@ import logging
 from .cluster import Cluster
 from .registry import Registry
 from .project import Project
+from .parameter_context import ParameterContext
 
 
 class Configuration:
 
-    def __init__(self, clusters: dict, registries: dict, projects: dict):
+    def __init__(self, clusters: dict, registries: dict, projects: dict, parameter_contexts: dict):
         self.clusters = [Cluster(name=c['name'], host_name=c['host_name'], security=c['security']) for c in clusters]
         self.registries = [Registry(name=r['name'], uri=r['host_name'], description=r['description']) for r in registries]
-        self.projects = [Project(name=p['name'], description=p['description'], registry_name=p['registry_name'], bucket_id=p['bucket_id'], flow_id=p['flow_id'], clusters=p['clusters']) for p in projects]
+        self.projects = [
+            Project(
+                name=p['name'],
+                description=p['description'],
+                registry_name=p['registry_name'],
+                bucket_id=p['bucket_id'],
+                flow_id=p['flow_id'],
+                clusters=p['clusters'])
+            for p in projects
+        ]
+        self.parameter_contexts = [
+            ParameterContext(
+                name=pc['name'],
+                description=pc['description'],
+                is_coordinated=pc['is_coordinated'],
+                parameters=pc['parameters'] if 'parameters' in pc else [])
+            for pc in parameter_contexts
+        ]
 
 
 def load_from_file(config_file_location: str) -> Configuration:
@@ -35,7 +53,8 @@ def _build_configuration(config_definition) -> Configuration:
         config = Configuration(
             config_definition['clusters'],
             config_definition['registries'],
-            config_definition['projects'])
+            config_definition['projects'],
+            config_definition['parameter_contexts'])
         return config
     except Exception as e:
         logging.critical(f'Error parsing configuration file: {e}')
---
+++ nifi_cluster_coordinator/configuration/parameter_context.py
@@ -0,0 +1,24 @@
+class Parameter:
+
+    def __init__(self, name, description, is_sensitive, value):
+        self.name = name
+        self.description = description
+        self.is_sensitive = is_sensitive
+        self.value = value
+
+
+class ParameterContext:
+
+    def __init__(self, name, description, is_coordinated, parameters):
+        self.name = name
+        self.description = description
+        self.is_coordinated = is_coordinated
+        self.parameters = [
+            Parameter(
+                name=p['name'],
+                description=p['description'],
+                is_sensitive=p['is_sensitive'],
+                value=p['value'])
+            for p in parameters
+        ]
+        self.id = None
